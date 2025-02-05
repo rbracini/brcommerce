ce9117f5e936ddb8ec495422ed2e4fc72bf61886
Regis Bracini <regis.bracini@gmail.com>
2025-02-05 16:32:53 -0300

    Adicionando classe de associação e ajustando Order e Product

--- src/main/java/tech/curtiu/brcommerce/entities/Order.java
+++ src/main/java/tech/curtiu/brcommerce/entities/Order.java
@@ -1,6 +1,10 @@
 package tech.curtiu.brcommerce.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -33,6 +37,9 @@ public class Order {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {
     }
@@ -83,6 +90,14 @@ public class Order {
 
     public void setPayment(Payment payment) {
         this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts() {
+        return items.stream().map(OrderItem::getProduct).toList();
     }
 
 }
